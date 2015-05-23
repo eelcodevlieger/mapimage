85599118de52ee4edcc9b99c6d636e703e4a4379
Eelco de Vlieger <eelcodevlieger@hotmail.com>
2015-05-23 21:02:52 +0100

    initial commit - continued

---
+++ src/test/java/com/knocks_for_no_meter/ImageGenerationTest.java
@@ -0,0 +1,99 @@
+package com.knocks_for_no_meter;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.knocksfornometer.mapimage.Candidate;
+import com.knocksfornometer.mapimage.Candidates;
+import com.knocksfornometer.mapimage.imagegeneration.ConstituencyVoteDistributionImageGeneratorExact;
+import com.knocksfornometer.mapimage.imagegeneration.ImageGenerator;
+
+public class ImageGenerationTest {
+
+	private static final Candidate CANDIDATE_CON = new Candidate(Color.BLUE, 30);
+	private static final Candidate CANDIDATE_LAB = new Candidate(Color.RED, 20);
+	private static final Candidate CANDIDATE_SNP = new Candidate(Color.YELLOW, 10);
+	private static final Candidate CANDIDATE_OTHER = new Candidate(Color.WHITE, 5);
+
+	private static Candidates candidates;
+	private static Candidate noVoteCandidate;
+	
+	private WritableRaster raster;
+	private ImageGenerator imageGenerator;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		List<Candidate> candidateList = new ArrayList<>( asList(CANDIDATE_CON, CANDIDATE_LAB, CANDIDATE_SNP, CANDIDATE_OTHER) );
+		noVoteCandidate = Candidate.createNoVoteCandidate(candidateList);
+		candidateList.add(noVoteCandidate);
+		candidates = new Candidates( candidateList.toArray( new Candidate[candidateList.size()] ) );
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		raster = new BufferedImage(200, 500, BufferedImage.TYPE_INT_ARGB).getRaster();
+		imageGenerator = new ConstituencyVoteDistributionImageGeneratorExact(candidates);
+	}
+
+	@Test(expected=IllegalStateException.class)
+	public void testImagePixelCountNotMultipleOfHundred() {
+		imageGenerator.generate( new BufferedImage(10, 5, BufferedImage.TYPE_INT_ARGB).getRaster() );
+	}
+	
+	@Test
+	public void testNoVoteCandidatePercentageCalculation() {
+		int noVotePercentage = 100;
+		for (Candidate candidate : candidates.getCandidates())
+			if(candidate != noVoteCandidate)
+				noVotePercentage -= candidate.getPercentage();
+		
+		assertEquals(noVoteCandidate.getPercentage(), noVotePercentage);
+	}
+	
+	@Test
+	public void testGenerateImage() {
+		imageGenerator.generate(raster);
+
+		int width = raster.getWidth();
+		int height = raster.getHeight();
+		int pixelCount = width * height;
+		
+		Map<Color, AtomicInteger> pixelColorCountMap = new HashMap<>();
+		int[] argbColor = new int[4];
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				raster.getPixel(x, y, argbColor);
+				Color pixelColor = new Color(argbColor[0], argbColor[1], argbColor[2], argbColor[3]);
+				AtomicInteger count = pixelColorCountMap.get(pixelColor);
+				if(count == null){
+					count = new AtomicInteger(1);
+					pixelColorCountMap.put(pixelColor, count);
+				}else{
+					count.incrementAndGet();
+				}
+			}	
+		}
+
+		for (Candidate candidate : candidates.getCandidates()) {
+			AtomicInteger pixelColorCount = pixelColorCountMap.remove( candidate.getColor() );
+			assertNotNull("Can't find pixelColorCount in map [candidate=" + candidate + ", pixelColorCountMap=" + pixelColorCountMap + "]", pixelColorCount);
+			int actualPixelCount = (int) (pixelColorCount.get() / (double)pixelCount * 100);
+			assertEquals("pixel percentage incorrect [candidate=" + candidate + "]", candidate.getPercentage(), actualPixelCount);
+		}
+	}
+
+}
